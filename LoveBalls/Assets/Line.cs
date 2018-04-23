bfddfafccae3daf90ddf3c61156f9cf4b70c04b8
jackgiorgio <418682632@qq.com>
2018-04-23 17:53:12 +0800

    LoveBalls

---
+++ LoveBalls/Assets/Line.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCol;
+
+    List<Vector2> points;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void UpdateLine(Vector2 mousePos)  // 将鼠标位置加入list<Vector2> points
+    {
+        if (points == null)
+        {
+            points = new List<Vector2>();
+            SetPoint(mousePos);
+            return;
+        }
+
+        if (Vector2.Distance(points.Last(), mousePos)> .1f)
+        {
+            SetPoint(mousePos);
+        }
+            
+
+        //Check if the mouse has move enough for us to insert new point
+        //if it has: Insert point at mouse position
+    }
+
+    void SetPoint(Vector2 point)
+    {
+        points.Add(point);
+        lineRenderer.positionCount = points.Count;
+        lineRenderer.SetPosition(points.Count - 1, point);
+
+
+        if (points.Count > 1)
+        {          
+            edgeCol.points = points.ToArray();
+        }
+    }
+}
---
+++ LoveBalls/Assets/LineCreator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour {
+
+    public GameObject linePrefab;
+
+
+    Line activeLine;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject lineGo = Instantiate(linePrefab);
+            activeLine = lineGo.GetComponentInChildren<Line>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Rigidbody2D rb = activeLine.transform.GetComponentInParent<Rigidbody2D>();
+            rb.isKinematic = false;
+            activeLine = null;
+        }
+
+        if (activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePos);
+        }
+	}
+}
