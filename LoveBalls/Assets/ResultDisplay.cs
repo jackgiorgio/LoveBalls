1e5d71e326d2b180ab0394f98b741eea800a02a6
jackgiorgio <418682632@qq.com>
2018-04-26 02:12:21 +0800

    Handle-Win-Lose-Condition

---
+++ LoveBalls/Assets/ResultDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultDisplay : MonoBehaviour {
+
+    public GameObject retryButton;
+    public GameObject NextButton;
+    private Animator animator;
+
+    // Use this for initialization
+    void Start() {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void DisplayWinPanel()
+    {
+        //get screenshot of the gameplay;
+        retryButton.SetActive(false);
+        NextButton.SetActive(true);
+        animator.SetTrigger("3Stars");
+    }
+
+    public void DisplayLosePanel()
+    {
+        retryButton.SetActive(true);
+        NextButton.SetActive(false);
+    }
+}
--- LoveBalls/Assets/Scripts/GameManager.cs
+++ LoveBalls/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour {
     public bool isLevelPassed = false;
     public bool isLose = false;
     private GameObject winPanel;
+    private ResultDisplay resultDisplay;
     
     // Use this for initialization
 	void Start () {
@@ -27,6 +28,7 @@ public class GameManager : MonoBehaviour {
             Debug.Log("LevelPassed!");
             //make everything kinematic;
             winPanel.SetActive(true); // display win panel, which will have a win animation and music play;
+            resultDisplay.DisplayWinPanel();
             isLevelPassed = false;
         }
     }
@@ -37,6 +39,7 @@ public class GameManager : MonoBehaviour {
         {
             Debug.Log("You Lose!");
             winPanel.SetActive(true); // display win panel, which will have a win animation and music play;
+            resultDisplay.DisplayLosePanel();
             isLose = false;
         }
     }
@@ -44,6 +47,7 @@ public class GameManager : MonoBehaviour {
     void FindWinPanel()
     {
         winPanel = GameObject.Find("WinPanel");
+        resultDisplay = GameObject.FindObjectOfType<ResultDisplay>();
         if (!winPanel)
         {
             Debug.LogError("Please create you win object");
