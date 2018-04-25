d32c27e6bef4d66c473c429fb2c7eec0063f62b7
jackgiorgio <418682632@qq.com>
2018-04-25 15:36:22 +0800

    ReMakeScenes

---
+++ LoveBalls/Assets/Scripts/FlashDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlashDisplay : MonoBehaviour {
+
+    public GameObject thingToBeFlash;
+
+    // Use this for initialization
+    void Start() {
+        InvokeRepeating("FlashTheObject", 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonUp(0))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    void FlashTheObject()
+    {
+        if (thingToBeFlash.activeInHierarchy)
+        {
+            thingToBeFlash.SetActive(false);
+        }
+        else
+        {
+            thingToBeFlash.SetActive(true);
+        }
+    }
+}
