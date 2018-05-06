0839dc85021ff686fed646b4f3975897598a7209
jackgiorgio <418682632@qq.com>
2018-05-07 04:14:57 +0800

    ShopSystem-version12

---
+++ LoveBalls/Assets/Scripts/Pen.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Pen", menuName = "Pen")]
+public class Pen : ScriptableObject {
+
+    public new string name;
+    public int price;
+    public Sprite penSprite;
+}
